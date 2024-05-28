bf0b8b2dd07130615932ff3d9fab93f5651183a1
Utkarsh Singh <utkarshsingh102005@gmail.com>
2024-05-28 10:39:55 +0530

    Learning

---
+++ do_while_loop.java
@@ -0,0 +1,9 @@
+public class do_while_loop {
+    public static void main(String[] args) {
+        int counter = 1;
+        do{
+            System.out.println("Hellow world");
+            counter++;
+        }while(counter<=10);
+    }
+}
