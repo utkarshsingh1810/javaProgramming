0615f27a3590445f2b8282f07d401b85f066841d
Utkarsh Singh <utkarshsingh102005@gmail.com>
2024-05-28 12:08:39 +0530

    learning 02

---
+++ break_conditional.java
@@ -0,0 +1,11 @@
+public class break_conditional {
+    public static void main(String[] args) {
+        for(int i =1; i<=5; i++){
+            if(i==3){
+                break;
+            }System.out.println(i);
+        }   //System.out.println(i);
+        
+        System.out.println("I am out of loop");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# I am learning java
+This repository is for learning purpose
+
+## Give a star to my repo
